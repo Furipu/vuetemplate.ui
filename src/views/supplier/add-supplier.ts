7cefcb90290d2253652f5e1348eb6d32974ca180
Philippe Van Damme <vandamme.philippe@outlook.be>
2021-11-11 16:23:09 +0100

    add supplier vue

---
+++ src/views/supplier/add-supplier.ts
@@ -0,0 +1,11 @@
+import { Vue } from "vue-class-component";
+import Component from "vue-class-component";
+import { client } from "@/services/index";
+import { AddSupplierDto } from "@/services/client";
+
+export default class AddSupplier extends Vue{
+    
+    async AddSupplierDto(): Promise<void>{
+
+    }
+}
